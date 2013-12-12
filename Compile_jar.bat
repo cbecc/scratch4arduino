b5521055c16ce0d0a9e4f8238b8206f6cc99da49
Claudio Becchetti <claudio.becchetti@gmail.com>
2013-12-12 23:32:02 +0100

    now works with any java version

--- Compile_jar.bat
+++ Compile_jar.bat
@@ -1,4 +1,4 @@
-"D:\Program Files (x86)\Java\jdk1.7.0_45\bin\javac" -classpath RXTXcomm.jar A4S.java processing/src/Firmata.java
+"D:\Program Files (x86)\Java\jdk1.7.0_45\bin\javac" -source 1.4 -classpath RXTXcomm.jar A4S.java processing/src/Firmata.java
 pause
 mkdir org\firmata
 pause
